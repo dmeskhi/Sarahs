a6808758aed5fe42a974121e14330a1f6467fdd0
dmeskhi <dmeskh@gmail.com>
2017-10-25 21:09:12 -0700

    dmeskhi

--- js/code.js
+++ js/code.js
@@ -1,15 +1,14 @@
-/* On Scroll Appearing Sticky Header */
-(function($) {          
-    $(document).ready(function(){                    
-        $(window).scroll(function(){                          
-            if ($(this).scrollTop() > 300) {
-                $('.headerafter').slideDown("slow");
-            } else {
-                $('.headerafter').slideUp("slow");
-            }
-        });
-    });
-})(jQuery);
+$(function(){
+  $(window).scroll(function() {
+     if($(window).scrollTop() >= 100) {
+       $('header').addClass('scrolled');
+     }
+    else {
+      $('header').removeClass('scrolled');
+    }
+  });
+});
+
 
 $('.logo-handle').on('click', function(){
 	$('ul').toggleClass('show');
